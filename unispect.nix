a763719bc48ddc96cc5a5e33f25fae21d109ae2c
xenia <xenia.agda@gmail.com>
2023-08-13 14:02:04 +0200

    Don't import nixpkgs

--- unispect.nix
+++ unispect.nix
@@ -1,7 +1,7 @@
-with (import <nixpkgs> {});
+{ rustPlatform, ... }:
 rustPlatform.buildRustPackage {
-    pname = "unispect" ;
-    version = "0.1" ;
-    src = ./. ;
-    cargoLock = { lockFile = ./Cargo.lock ; } ;
+  pname = "unispect" ;
+  version = "0.1" ;
+  src = ./. ;
+  cargoLock = { lockFile = ./Cargo.lock ; } ;
 }
